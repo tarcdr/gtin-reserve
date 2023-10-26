fd70f8a6feffdc67174cfd893a49b94c5b33492a
Thiti Chompha <thiti@doctoratoz.co>
2023-10-26 23:26:22 +0700

    add gitignore

--- public/build/assets/ApplicationLogo-dec9c966.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-7dd90f70.js";import{l as t}from"./logo-deb6a867.js";function p(o){return r.jsx("img",{...o,src:t,viewBox:"0 0 316 316",alt:"Logo Company"})}export{p as A};
--- public/build/assets/logo-deb6a867.js
+++
@@ -1,1 +0,0 @@
-const o="/build/assets/logo-ef2c88f5.png";export{o as l};
