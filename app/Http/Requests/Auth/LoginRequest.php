b5cbaffb8aa227292047c640c86e0b12818bb025
Thiti Chompha <thiti@doctoratoz.co>
2023-10-26 22:09:28 +0700

    dd

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -41,7 +41,7 @@ class LoginRequest extends FormRequest
     {
         $this->ensureIsNotRateLimited();
 
-        if (! Auth::attempt(true, $this->boolean('remember'))) {
+        if (! Auth::attempt($this->only('user_login', 'pass_login'), $this->boolean('remember'))) {
             RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([
