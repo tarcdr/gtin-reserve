f5c9841c98f26e7aebc5decd33bc0d436394172d
Thiti Chompha <thiti@doctoratoz.co>
2023-10-29 21:30:12 +0700

    dd

--- app/Http/Controllers/RequestFormController.php
+++ app/Http/Controllers/RequestFormController.php
@@ -81,7 +81,7 @@ class RequestFormController extends Controller
         $p_brand_abb    = 'NULL';
         $l_product_code = 'NULL';
         $s_product_code = 'NULL';
-        $p_msg          = 'NULL';
+        $p_msg          = "";
 
         // $procedureName = 'proj1_gen_mattype';
 
