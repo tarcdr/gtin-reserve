980f79d28a3324f80ea8e6e80b3082173dad48f3
Thiti Chompha <thiti@doctoratoz.co>
2023-11-01 00:25:56 +0700

    ddd

--- app/Http/Controllers/RequestFormController.php
+++ app/Http/Controllers/RequestFormController.php
@@ -54,7 +54,6 @@ class RequestFormController extends Controller
 
         $stmt = $pdo->prepare("begin program2(:p3, :p4); end;");
         $stmt->bindParam(':p3', $p1, PDO::PARAM_INT);
-        $stmt->bindParam(':p4', $p4, PDO::PARAM_INT|PDO::PARAM_INPUT_OUTPUT, 40);
         $stmt->bindValue(':p4', $p4, PDO::PARAM_INT);
         $stmt->execute();
 
