107952e14b0f57a2550bcb99d442aad180612518
Thiti Chompha <thiti@doctoratoz.co>
2023-11-28 02:08:51 +0700

    ddd

--- app/Http/Controllers/RequestFormController.php
+++ app/Http/Controllers/RequestFormController.php
@@ -38,6 +38,63 @@ class RequestFormController extends Controller
         $p_new_last_gtin_box = $request->p_new_last_gtin_box;
         $p_suggest_gtin_box  = $request->p_suggest_gtin_box;
 
+        if ($request->material_id) {
+          $db = '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=' . $host . ')(PORT=' . $port . '))(CONNECT_DATA=(SERVICE_NAME = ' . $database . ')))';
+          $conn = oci_connect($username, $password, $db);
+
+          $p_trading_unit_pcs = 'Pcs';
+          $p_trading_unit_box = $request->trading_unit;
+          $p_gtin_pcs         = NULL;
+          $p_gtin_box         = NULL;
+          $p_user_login       = $request->user()->user_login;
+          if ($request->gtinExistPcs && $request->gtinPcsCode) {
+            $p_gtin_pcs = $request->gtinPcsCode;
+          } else if ($request->gtinPcsChoose === 'l') {
+            $p_gtin_pcs = $p_new_last_gtin_pcs;
+          } else if ($request->gtinPcsChoose === 's') {
+            $p_gtin_pcs = $p_suggest_gtin_pcs;
+          }
+          if ($request->gtinExistPack && $request->gtinPackCode) {
+            $p_gtin_box = $request->gtinPackCode;
+          } else if ($request->gtinPackChoose === 'l') {
+            $p_gtin_box = $p_new_last_gtin_box;
+          } else if ($request->gtinPackChoose === 's') {
+            $p_gtin_box = $p_suggest_gtin_box;
+          }
+          if ($p_gtin_pcs || $p_gtin_box) {
+            if (!$conn) {
+                $e = oci_error();
+                trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+            }
+  
+            $stid0 = oci_parse($conn, 'begin proj1_find_newgtin(:p_material_id, :p_trading_unit_pcs, :p_trading_unit_box, :p_gtin_pcs, :p_gtin_box, :p_user_login); end;');
+            oci_bind_by_name($stid0, ':p_material_id',      $request->material_id);
+            oci_bind_by_name($stid0, ':p_trading_unit_pcs', $p_trading_unit_pcs);
+            oci_bind_by_name($stid0, ':p_trading_unit_box', $p_trading_unit_box);
+            oci_bind_by_name($stid0, ':p_gtin_pcs',         $p_gtin_pcs);
+            oci_bind_by_name($stid0, ':p_gtin_box',         $p_gtin_box);
+            oci_bind_by_name($stid0, ':p_user_login',       $p_user_login);
+    
+            oci_execute($stid0);
+          }
+
+          foreach (Gtin::where('material_id', $request->material_id)->orderBy('global_trade_item_number')->get() as $m) {
+            array_push($gtins, $m);
+          }
+          if (!$conn) {
+              $e = oci_error();
+              trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+          }
+
+          $stid = oci_parse($conn, 'begin proj1_find_newgtin(:p_new_last_gtin_pcs, :p_suggest_gtin_pcs, :p_new_last_gtin_box, :p_suggest_gtin_box); end;');
+          oci_bind_by_name($stid, ':p_new_last_gtin_pcs', $p_new_last_gtin_pcs, 100);
+          oci_bind_by_name($stid, ':p_suggest_gtin_pcs',  $p_suggest_gtin_pcs,  100);
+          oci_bind_by_name($stid, ':p_new_last_gtin_box', $p_new_last_gtin_box, 100);
+          oci_bind_by_name($stid, ':p_suggest_gtin_box',  $p_suggest_gtin_box,  100);
+  
+          oci_execute($stid);
+        }
+
         foreach (Material::select('brand')->whereNotNull('brand')->groupBy('brand')->orderBy('brand')->get() as $b) {
           array_push($brand, [
             "code" => $b->brand
@@ -60,25 +117,6 @@ class RequestFormController extends Controller
             "unit" => $m->trading_unit,
             "type" => $m->type_gtin_unit,
           ]);
-        }
-        if ($request->material_id) {
-          foreach (Gtin::where('material_id', $request->material_id)->orderBy('global_trade_item_number')->get() as $m) {
-            array_push($gtins, $m);
-          }
-          $db = '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=' . $host . ')(PORT=' . $port . '))(CONNECT_DATA=(SERVICE_NAME = ' . $database . ')))';
-          $conn = oci_connect($username, $password, $db);
-          if (!$conn) {
-              $e = oci_error();
-              trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
-          }
-
-          $stid = oci_parse($conn, 'begin proj1_find_newgtin(:p_new_last_gtin_pcs, :p_suggest_gtin_pcs, :p_new_last_gtin_box, :p_suggest_gtin_box); end;');
-          oci_bind_by_name($stid, ':p_new_last_gtin_pcs', $p_new_last_gtin_pcs, 100);
-          oci_bind_by_name($stid, ':p_suggest_gtin_pcs',  $p_suggest_gtin_pcs,  100);
-          oci_bind_by_name($stid, ':p_new_last_gtin_box', $p_new_last_gtin_box, 100);
-          oci_bind_by_name($stid, ':p_suggest_gtin_box',  $p_suggest_gtin_box,  100);
-  
-          oci_execute($stid);
         }
 
         return Inertia::render('Request', [
