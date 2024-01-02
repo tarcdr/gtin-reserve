dfe093e2b15d69b29173ac3e303ab1ad2d1624b9
Thiti Chompha <thiti@doctoratoz.co>
2024-01-02 22:57:53 +0700

    ddd

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+use Illuminate\Http\Request;
 
 class VerifyCsrfToken extends Middleware
 {
@@ -14,4 +15,11 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         //
     ];
+    /**
+     * Get the path the user should be redirected to when they are not authenticated.
+     */
+    protected function redirectTo(Request $request): ?string
+    {
+        return $request->expectsJson() ? null : route('login');
+    }
 }
