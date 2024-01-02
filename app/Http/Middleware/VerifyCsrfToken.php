2d96a4d8fefe37fb99109c7ffedeaa30dca2518c
Thiti Chompha <thiti@doctoratoz.co>
2024-01-02 23:36:56 +0700

    ddd

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Middleware;
 
-use Closure;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
-use Illuminate\Support\Facades\Auth;
 
 class VerifyCsrfToken extends Middleware
 {
@@ -16,14 +14,4 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         //
     ];
-
-    public function handle($request, Closure $next)
-    {
-        if($request->route()->named('logout')) {
-            if (!Auth::check() || Auth::guard()->viaRemember()) {
-                $this->except[] = route('logout');
-            }
-        }
-        return parent::handle($request, $next);
-    }
 }
