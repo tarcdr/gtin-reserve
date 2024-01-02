7485aec6acefd5411f047e7bca5f59091177564f
Thiti Chompha <thiti@doctoratoz.co>
2024-01-02 23:11:05 +0700

    dd

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -28,7 +28,7 @@ class Handler extends ExceptionHandler
         });
         $this->renderable(function (\Exception $e) {
             if ($e->getPrevious() instanceof \Illuminate\Session\TokenMismatchException) {
-                return redirect()->route('login');
+                return redirect('login');
             };
         });
     }
