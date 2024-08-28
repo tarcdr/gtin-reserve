4e5046f8e596edeaa6a5ffd071f28c7e80b90cc6
tarcdr <tarcdr@gmail.com>
2024-08-28 13:17:28 +0000

    production

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     server: {
         host: '0.0.0.0',
         hmr: {
-            host: 'localhost'
+            host: '172.16.20.121'
         }
     },
     plugins: [
